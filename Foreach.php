f3f4f12b3534ce0e53bbadce098ba1fee1d77adc
Rafael Felipe <57761831+Miopiaa@users.noreply.github.com>
2021-12-10 11:59:40 -0300

    Add files via upload

---
+++ Foreach.php
@@ -0,0 +1,16 @@
+<?php
+    $arr = array('guilherme', 'joao', 'felipe', 'mario');
+/*
+    foreach ($arr as $key => $value) {
+        echo $key;
+        echo ' => ';
+        echo $value; 
+        echo '<hr>';
+    }
+*/
+    $total  = count($arr);
+    for ($i=0; $i<$total; $i++){
+        echo $arr[$i];
+        echo '<br>';
+    }
+?>
---
+++ laço.php
@@ -0,0 +1,26 @@
+<?php
+/* laço for
+    for ($cont=0;$cont<10;$cont++){
+        echo 'Olá, mundo';
+        echo '<hr>';
+    }
+*/
+
+/*  laço while 
+    $cont = 0;
+    while ($cont < 10){
+        echo 'alo mundo';
+        echo '<hr>';
+        $cont+=1;
+    }
+*/
+
+/*  while que executa antes de conferir
+    $cont = 0;
+    do{
+        echo 'ola mundo';
+        echo '<br>';
+        $cont++;
+    }while($cont < 10);
+*/
+?>
