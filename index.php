a9548d6902b50f95237efe2db517f8e32037f504
Rafael Felipe <57761831+Miopiaa@users.noreply.github.com>
2021-12-29 17:54:46 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
 <?php
-    
-include ('config.php');
+ini_set('display_errors', 1);
+$pdo = new PDO('mysql:host=localhost;dbname=modulo_8','root','198866');
 
-new utilidades();
+$sql = $pdo->prepare("SELECT * FROM posts");
 
-new Home\Inicial();
+$sql->execute();
+
+$info = $sql->fetchAll();
+/*
+foreach ($info as $Key => $value){
+    echo 'Titulo '.$value['titulo'];
+    echo '<br>';
+    echo 'Noticia: '.$value['conteudo'];
+    echo '<hr>';
+}
+
+for ($i = 0;$i < count($info);$i++){
+    echo 'Titulo '.$info[$i]['titulo'];
+    echo '<br>';
+    echo 'Noticia: '.$info[$i]['conteudo'];
+    echo '<hr>';
+}
+*/
+
+
+
 ?>
