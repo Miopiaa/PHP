21d66de03b373b382d916ead0b6ee5a288796a9f
Rafael Felipe <57761831+Miopiaa@users.noreply.github.com>
2022-01-04 12:47:39 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,45 @@
-<?php
-    define('HOST','localhost');
-    define('DB','modulo_8');
-    define('USER','root');
-    define('PASS','198866');
+<!--
+<form method="post">
+    <input type="text" name="adress" />
+    <input type="submit" name="acao" value="Enviar" />
+</form>
+-->
 
-     try{
-        $pdo = new PDO('mysql:host='.HOST.';dbname='.DB,USER,PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    }catch(Exception $e){
-        echo 'erro ao conectar';
+<?php
+    //aula CURL
+    $endereco = urlencode('coqueiros');
+    $curl = curl_init();
+
+    curl_setopt($curl, CURLOPT_URL,"http://maps.googleapis.com/maps/api/geocode/json?address=$endereco");
+    //curl_setopt($curl, CURLOPT_POST, 1);
+    //curl_setopt($curl, CURLOPT_POSTFIELDS,http_build_query(array()));
+
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    $server_output = curl_exec($curl);
+
+    curl_close($curl);
+
+    echo $server_output;
+    //requisicao com CURL no server local
+    /*
+    $resultado = json_decode($server_output);
+
+    echo $resultado->titulo[0];
+    echo '<br>';
+    echo $resultado->conteudo[0];
+    /*
+    //POR ALGUMA REZAO A API RECUSA ACESSO!!!!!!
+    if(isset($_POST['acao'])){
+    //Requsição para o google utilizando file get contents
+    $url = urlencode($_POST['adress']);
+    $str = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address='.$url.'&sensor=false&key=AppKey(AIzaSyANjztPEDsedIgFN_iMDKC6UFPoCC2HYn8)');
+
+    //https://maps.googleapis.com/maps/api/distancematrix/json?origins=dehli&destinations=pune&mode=bicycling&language=en-EN&sensor=true&key=AppKey(paste App Key)
+
+    $endereco = json_decode($str);
+    print_r($endereco);
+    echo $endereco->results[0]->address_components[0]->short_name;
     }
+    */
 ?>
--- request.php
+++ request.php
@@ -1,7 +1,9 @@
 <?php
-$arr = ['nome'=>'Guilherme','cidade'=>'floripa'];
-
-$json = json_encode($arr);
-echo $json;
+    if(isset($_POST['request']) && $_POST['request'] == 'name_return'){
+        //die(json_encode(array('resultado'=>array('outro_objeto'=>'Guilherme'))));
+        //seleciona as noticias;
+        $noticias = array('titulo'=>array('noticia 1', 'notica 2'), 'conteudo'=>array('conteudo da notica 1','conteudo da notica 2'));
+        die(json_encode($noticias));
+    }
 
 ?>
